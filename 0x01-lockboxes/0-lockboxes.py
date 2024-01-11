a2c9f7797f7699b037d17c6564b24933faf6186a
WinnyKay3 <winffredkalondu@gmail.com>
2024-01-11 13:51:29 +0000

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 '''Write a method that determines if all the boxes can be opened'''
 
+
 def canUnlockAll(boxes):
-  '''Check if all boxes can be unlocked
-  :param boxes:
-  :return: True or False
-  '''
-  
-  if not boxes or type(boxes) is not list:
+    '''Check if all boxes can be unlocked
+    :param boxes:
+    :return: True or False
+    '''
+
+    if not boxes or type(boxes) is not list:
+        return False
+
+    unlocked = [0]
+    for n in unlocked:
+        for key in boxes[n]:
+            if key not in unlocked and key < len(boxes):
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+
     return False
-  
-  unlocked = [0]
-  for n in unlocked:
-    for key in boxes[n]:
-        if key not in unlocked and key < len(boxes):
-          unlocked.append(key)
-  if len(unlocked) == len(boxes):
-      return True
-    
-  return False
