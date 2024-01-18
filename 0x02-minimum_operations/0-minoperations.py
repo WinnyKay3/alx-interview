ab7407f588b33404d538d5008b0ff73a24d1daa6
WinnyKay3 <winffredkalondu@gmail.com>
2024-01-19 00:46:28 +0300

    .\0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,16 +13,15 @@ def minOperations(n):
         or 0 if n is impossible to achieve.
     """
     if n < 2:
-      return 0
-    
+        return 0
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
+
     return operations
-    
